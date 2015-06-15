00be4dce65172676df84e8c76b3ed62603bf02c2
James Humphries <jhumphries@vqcomms.com>
2015-06-15 13:35:26 +0100

    Added port mappings

---
+++ Src/Jetty/Models/ContainerHostConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Jetty
+{
+    public class ContainerHostConfig
+    {
+        public Dictionary<string,PortMapping> PortBindings { get; set; }
+        public bool PublishAllPorts { get; set; }
+
+        public ContainerHostConfig(Dictionary<int, int> portBindings, bool publishAllPorts = false)
+        {
+            this.PublishAllPorts = publishAllPorts;
+            this.PortBindings = new Dictionary<string,PortMapping>();
+            foreach (var binding in portBindings)
+            {
+                this.PortBindings.Add(string.Format("{0}/tcp",binding.Key), new PortMapping(binding.Value));
+            }
+        }
+    }
+}
+
--- samples/HelloWorld/HelloWorld/Program.cs
+++ samples/HelloWorld/HelloWorld/Program.cs
@@ -2,6 +2,7 @@
 using Jetty;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace HelloWorld
 {
@@ -18,7 +19,8 @@ namespace HelloWorld
             //list all containers
             var allContainers = client.ListContainers(allContainers: true).Result;
 
-            var container = allContainers.FirstOrDefault(x => x.Image == "hello-world:latest");
+            var container = allContainers.FirstOrDefault(x => x.Image == "hello-world");
+
             if (container == null)
             {
                 //Find image locally or pull it - this returns a list of statuses from creating it
@@ -26,10 +28,10 @@ namespace HelloWorld
    
                 Console.WriteLine("Downloading hello-world image");
 
-                if (data.Any(x => x.Status == "Download complete"))
+                if (data.Any(x => x.Status == "Download complete") || data.Any(x => x.Status.Contains("Image is up to date")))
                 {
                     //create the container
-                    var containerResult = client.CreateContainer(new CreateContainerOptions("hello-world", true, new [] { 80, 443 })).Result;
+                    var containerResult = client.CreateContainer(new CreateContainerOptions("hello-world", true)).Result;
                     containerId = containerResult.Id;
                 }
             }
@@ -37,13 +39,19 @@ namespace HelloWorld
             {
                 containerId = container.Id;  
             }
-
+ 
             Console.WriteLine("Starting hello-world image");
 
+            //host : client
+            var PortBindings = new Dictionary<int,int>();
+            PortBindings.Add(80, 80);
+            PortBindings.Add(443, 443);
+
             //start the container we just created
-            var containerStartResponse = client.StartContainer(containerId).Result;
+            var containerStartResponse = client.StartContainer(containerId, new ContainerHostConfig(PortBindings)).Result;
 
             Console.WriteLine("Press any key to kill it");
+
             Console.ReadKey();
 
             client.KillContainer(containerId);
